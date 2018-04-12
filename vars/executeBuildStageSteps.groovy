6fd2b4ef2eca20e7c83406d025918c54dd3c569a
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-12 16:59:58 -0700

    testing

--- vars/executeBuildStageSteps.groovy
+++ vars/executeBuildStageSteps.groovy
@@ -4,15 +4,15 @@ def call() {
     def stageName = 'Build'
     echo("Executing [${stageName}] stage steps...")
 
-    def javaHome = tool('jdk8')
+    def javaHome = tool('jdk8u162')
     def mvnHome = tool('maven3')
 
-    /*env.JAVA_HOME="${javaHome}"
+    env.JAVA_HOME="${javaHome}"
     env.MAVEN_HOME="${mvnHome}"
     env.PATH="${env.JAVA_HOME}/bin:${env.MAVEN_HOME}/bin:${env.PATH}"
-    echo("Java Home: ${env.JAVA_HOME}, Maven Home: ${env.MAVEN_HOME}, Path: ${env.PATH}")*/
+    echo("Java Home: ${env.JAVA_HOME}\r\n\r\nMaven Home: ${env.MAVEN_HOME}\r\n\r\nPath: ${env.PATH}")
 
-    sh("mvn -v clean compile")
+    sh("${mvnHome}/bin/mvn -v clean compile")
 
     echo("Completed [${stageName}] stage steps.")
 }
