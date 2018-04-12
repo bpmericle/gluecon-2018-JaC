515913b2c60cc8eab8e02e6b09b3818a5a8e052a
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-12 16:56:14 -0700

    testing

--- vars/executeBuildStageSteps.groovy
+++ vars/executeBuildStageSteps.groovy
@@ -7,10 +7,10 @@ def call() {
     def javaHome = tool('jdk8')
     def mvnHome = tool('maven3')
 
-    env.JAVA_HOME="${javaHome}"
+    /*env.JAVA_HOME="${javaHome}"
     env.MAVEN_HOME="${mvnHome}"
     env.PATH="${env.JAVA_HOME}/bin:${env.MAVEN_HOME}/bin:${env.PATH}"
-    echo("Java Home: ${env.JAVA_HOME}, Maven Home: ${env.MAVEN_HOME}, Path: ${env.PATH}")
+    echo("Java Home: ${env.JAVA_HOME}, Maven Home: ${env.MAVEN_HOME}, Path: ${env.PATH}")*/
 
     sh("mvn -v clean compile")
 
