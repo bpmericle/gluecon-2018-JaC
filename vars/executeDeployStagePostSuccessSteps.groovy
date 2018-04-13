7a8c1709fd85108c5ed2b42cca4f1823bc34a7a9
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-13 16:37:58 -0700

    added reports b/c the security setting in Jenkins messes with the javadoc and xref when using frames.

--- vars/executeDeployStagePostSuccessSteps.groovy
+++ vars/executeDeployStagePostSuccessSteps.groovy
@@ -2,6 +2,8 @@
 
 def call() {
     def stageName = 'Deploy'
-    publishHTML([allowMissing: false, reportDir: 'target/site', reportFiles: 'index.html', reportName: 'Maven Generated Website'])
+    publishHTML([allowMissing: false, reportDir: 'target/site', reportFiles: 'index.html', reportName: 'Maven Site'])
+    publishHTML([allowMissing: false, reportDir: 'target/site/apidocs', reportFiles: 'overview-summary.html', reportName: 'Source Javadoc'])
+    publishHTML([allowMissing: false, reportDir: 'target/site/xref', reportFiles: 'overview-summary.html', reportName: 'Source Xref'])
     echo("Completed [${stageName}] stage with result [success].")
 }
