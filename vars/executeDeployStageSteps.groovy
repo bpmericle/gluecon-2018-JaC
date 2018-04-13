bc4513d06b175333b27089deebe0ae005eefd449
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-13 13:35:34 -0700

    Fix for missing javadoc

--- vars/executeDeployStageSteps.groovy
+++ vars/executeDeployStageSteps.groovy
@@ -4,7 +4,7 @@ def call() {
     def stageName = 'Deploy'
     echo("Executing [${stageName}] stage steps...")
 
-    sh('mvn -V -B -U -e site')
+    sh('mvn -V -B -U -e -Dskip.surefire.tests=true -Dskip.failsafe.tests=true verify site')
 
     echo("Completed [${stageName}] stage steps.")
 }
