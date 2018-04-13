d2ca92d47894a294db3c27a47ec3b0ffa8f37a9f
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-13 09:41:10 -0700

    Skip junit tests when running functional tests.

--- vars/executeFunctionalTestStageSteps.groovy
+++ vars/executeFunctionalTestStageSteps.groovy
@@ -4,7 +4,7 @@ def call() {
     def stageName = 'Functional Test'
     echo("Executing [${stageName}] stage steps...")
 
-    sh('mvn -V -B -U -e verify')
+    sh('mvn -V -B -U -e -DskipTests verify')
 
     echo("Completed [${stageName}] stage steps.")
 }
