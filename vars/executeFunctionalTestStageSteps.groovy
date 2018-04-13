0594ebfb18ab8b2235a7ac2cc3829c5d23eaef8f
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-13 10:58:22 -0700

    Skip unit tests when running integration test stage

--- vars/executeFunctionalTestStageSteps.groovy
+++ vars/executeFunctionalTestStageSteps.groovy
@@ -4,7 +4,7 @@ def call() {
     def stageName = 'Functional Test'
     echo("Executing [${stageName}] stage steps...")
 
-    sh('mvn -V -B -U -e -DskipTests -Dmaven.javadoc.skip=true -Dmaven.source.skip=true verify')
+    sh('mvn -V -B -U -e -Dskip.surefire.tests=true -Dmaven.javadoc.skip=true -Dmaven.source.skip=true verify')
 
     echo("Completed [${stageName}] stage steps.")
 }
