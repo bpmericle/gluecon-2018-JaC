38dc04f1aa02e1953e6ac1d48cab182cb0df7928
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-13 17:08:23 -0700

    removed the xref report

--- vars/executeDeployStagePostSuccessSteps.groovy
+++ vars/executeDeployStagePostSuccessSteps.groovy
@@ -3,7 +3,6 @@
 def call() {
     def stageName = 'Deploy'
     publishHTML([allowMissing: false, reportDir: 'target/site', reportFiles: 'index.html', reportName: 'Maven Site'])
-    publishHTML([allowMissing: false, reportDir: 'target/site/apidocs', reportFiles: 'overview-summary.html', reportName: 'Source Javadoc'])
-    publishHTML([allowMissing: false, reportDir: 'target/site/xref', reportFiles: 'overview-summary.html', reportName: 'Source Xref'])
+    publishHTML([allowMissing: false, reportDir: 'target/site/apidocs', reportFiles: 'overview-summary.html', reportName: 'Javadoc'])
     echo("Completed [${stageName}] stage with result [success].")
 }
