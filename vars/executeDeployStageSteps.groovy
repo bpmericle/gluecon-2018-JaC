2f0fc5baaf625d2393b4f53859ba51c41e95d930
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-16 16:44:51 -0700

    Inserted retry logic.

--- vars/executeDeployStageSteps.groovy
+++ vars/executeDeployStageSteps.groovy
@@ -7,12 +7,16 @@ def call() {
     def serverPort = 8080
     sh("mvn -Dserver.port=${serverPort} -V -e spring-boot:run &")
 
-    sleep(45)
-
-    echo('Testing health endpoint.')
-    def content = sh(script: "curl -X GET http://localhost:${serverPort}/actuator/health", returnStdout: true).trim()
-    echo("content: ${content}")
-    assert content.contains('{\"status\":\"UP\"}')
+    def retries = 4
+    def attempts = 0
+    def sleepInterval = 15
+    retry(retries) {
+        attempts++
+        sleep(sleepInterval)
+        echo("Testing health endpoint. Attempt ${attempts} out of ${retries}.")
+        def content = sh(script: "curl -X GET http://localhost:${serverPort}/actuator/health", returnStdout: true).trim()
+        assert content.contains('{\"status\":\"UP\"}')
+    }
 
     echo('Shutting down service.')
     sh("curl -i -X POST http://localhost:${serverPort}/actuator/shutdown")
