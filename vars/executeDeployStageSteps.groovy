a5d8f9705d0f0bc246d008f9c03cf393d6e117bd
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-16 16:17:33 -0700

    Debugging

--- vars/executeDeployStageSteps.groovy
+++ vars/executeDeployStageSteps.groovy
@@ -4,13 +4,14 @@ def call() {
     def stageName = 'Deploy'
     echo("Executing [${stageName}] stage steps...")
 
-    //sh('mvn -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -V -e verify site')
-    sh('mvn -Dserver.port=8080 -V -e spring-boot:run &')
+    def serverPort = 8090
+    def response = sh(script: "mvn -Dserver.port=${serverPort} -V -e spring-boot:run", returnStdout: true).trim()
+    echo("Output:\r\n${response}")
 
     sleep(30)
 
     echo('Testing health endpoint.')
-    def rawContent = sh(script: 'curl -i -X GET http://localhost:8080/actuator/health', returnStdout: true).trim()
+    def rawContent = sh(script: "curl -i -X GET http://localhost:${serverPort}/actuator/health", returnStdout: true).trim()
     echo("rawContent: ${rawContent}")
     def content = readJSON(rawContent)
     echo("content: ${content}")
@@ -18,7 +19,7 @@ def call() {
     assert status == 'UP'
 
     echo('Shutting down service.')
-    sh('curl -i -X POST http://localhost:8080/actuator/shutdown')
+    sh("curl -i -X POST http://localhost:${serverPort}/actuator/shutdown")
 
     echo("Completed [${stageName}] stage steps.")
 }
