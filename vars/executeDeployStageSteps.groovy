ca6d483357ee1716d7919340175ccf122ae0e5aa
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-16 15:47:31 -0700

    Added test of deployment.

--- vars/executeDeployStageSteps.groovy
+++ vars/executeDeployStageSteps.groovy
@@ -5,7 +5,18 @@ def call() {
     echo("Executing [${stageName}] stage steps...")
 
     //sh('mvn -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -V -e verify site')
-    sh('mvn -Dserver.port=8080 -V -e spring-boot:run')
+    sh('mvn -Dserver.port=8080 -V -e spring-boot:run &')
+
+    sleep(30)
+
+    echo('Testing health endpoint.')
+    def rawContent = new URL('http://localhost:8000/actuator/health').getText()
+    def content = readJSON(rawContent)
+    def status = content.get('status')
+    assert status == 'UP'
+
+    echo('Shutting down service.')
+    sh('curl -k -XPOST https://localhost:8000/actuator/shutdown')
 
     echo("Completed [${stageName}] stage steps.")
 }
