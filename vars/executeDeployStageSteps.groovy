8c0a0fc415e2a6711d4c5677f6148f198fe72749
Brian Mericle <brian_mericle@choicehotels.com>
2018-04-16 16:38:29 -0700

    Testing

--- vars/executeDeployStageSteps.groovy
+++ vars/executeDeployStageSteps.groovy
@@ -4,21 +4,15 @@ def call() {
     def stageName = 'Deploy'
     echo("Executing [${stageName}] stage steps...")
 
-    sh("mvn -Dserver.port=8080 -V -e spring-boot:run &")
+    def serverPort = 8080
+    sh("mvn -Dserver.port=${serverPort} -V -e spring-boot:run &")
 
     sleep(45)
 
-    sh('uname -s')
-    sh('ps -ax | grep java')
-
-    def serverPort = 8080
     echo('Testing health endpoint.')
-    def rawContent = sh(script: "curl -i -X GET http://localhost:${serverPort}/actuator/health", returnStdout: true).trim()
-    echo("rawContent: ${rawContent}")
-    def content = readJSON(rawContent)
+    def content = sh(script: "curl -X GET http://localhost:${serverPort}/actuator/health", returnStdout: true).trim()
     echo("content: ${content}")
-    def status = content.get('status')
-    assert status == 'UP'
+    assert content.contains('{\"status\":\"UP\"}')
 
     echo('Shutting down service.')
     sh("curl -i -X POST http://localhost:${serverPort}/actuator/shutdown")
