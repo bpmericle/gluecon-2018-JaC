c5e8922008f018d62ff345816377d85b771b8441
Brian Mericle <>
2018-05-15 23:08:45 -0600

    made all constants final

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -2,19 +2,19 @@
 
 import groovy.transform.Field
 
-@Field String STAGE_COMPILE = 'Compile'
-@Field String STAGE_UNIT_TESTS = 'Unit Tests'
-@Field String STAGE_STATIC_ANALYSIS = 'Static Analysis'
-@Field String STAGE_INTEGRATION_TESTS = 'Integration Tests'
-@Field String STAGE_PUBLISH_ARTIFACT = 'Publish Artifact'
-@Field String STAGE_DEPLOY_TO_PRE_PRODUCTION = 'Deploy to Pre-Production'
-@Field String STAGE_DEPLOY_TO_PRODUCTION_INPUT = 'Deploy to Production?'
-@Field String STAGE_DEPLOY_TO_PRODUCTION = 'Deploy to Production'
+@Field final String STAGE_COMPILE = 'Compile'
+@Field final String STAGE_UNIT_TESTS = 'Unit Tests'
+@Field final String STAGE_STATIC_ANALYSIS = 'Static Analysis'
+@Field final String STAGE_INTEGRATION_TESTS = 'Integration Tests'
+@Field final String STAGE_PUBLISH_ARTIFACT = 'Publish Artifact'
+@Field final String STAGE_DEPLOY_TO_PRE_PRODUCTION = 'Deploy to Pre-Production'
+@Field final String STAGE_DEPLOY_TO_PRODUCTION_INPUT = 'Deploy to Production?'
+@Field final String STAGE_DEPLOY_TO_PRODUCTION = 'Deploy to Production'
 
-@Field String STEP_NAME_COMPILE = 'Compile'
+@Field final String STEP_NAME_COMPILE = 'Compile'
 
-@Field String METRIC_NAME_EXECUTION_TIME = 'execution_time'
+@Field final String METRIC_NAME_EXECUTION_TIME = 'execution_time'
 
-@Field String STATUS_SUCCESS = 'success'
-@Field String STATUS_FAILURE = 'failure'
-@Field String STATUS_ABORTED = 'aborted'
+@Field final String STATUS_SUCCESS = 'success'
+@Field final String STATUS_FAILURE = 'failure'
+@Field final String STATUS_ABORTED = 'aborted'
