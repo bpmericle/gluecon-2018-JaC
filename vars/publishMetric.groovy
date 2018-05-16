e051434137cbd6c2c96681538e6c4d4e77a56d46
Brian Mericle <>
2018-05-15 23:35:43 -0600

    fixed var reference

--- vars/publishMetric.groovy
+++ vars/publishMetric.groovy
@@ -8,6 +8,6 @@ def call(metricName, value, tags=null) {
     defaultTags['job_name'] = JOB_NAME
     defaultTags['job_url'] = JOB_URL
 
-    echo("Publishing metric [${metricName}] with value [${value}] and tags [${tags}]...")
+    echo("Publishing metric [${metricName}] with value [${value}] and tags [${defaultTags}]...")
     //publish the metric to some time-series datasource (like InfluxDB)
 }
